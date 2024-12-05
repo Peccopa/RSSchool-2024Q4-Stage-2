a51db12752e2cdefd7ce4a741ca3bc21bdfc1e68
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-05 16:26:12 +0300

    fix: add fix timer count

--- christmas-shop/src/js/burger.js
+++ christmas-shop/src/js/burger.js
@@ -3,6 +3,7 @@ import { scroll } from './scroll.js';
 export const burger = {
   menu: document.querySelector('.burger'),
   nav: document.querySelector('.nav'),
+  sliderImages: document.querySelectorAll('.slider__img'),
   openBurgerMenu() {
     if (
       !burger.menu.classList.contains('burger_opened') &&
@@ -11,10 +12,18 @@ export const burger = {
       burger.menu.classList.add('burger_opened');
       burger.nav.classList.add('nav_opened');
       scroll.hideScroll();
+
+      if (burger.sliderImages)
+        burger.sliderImages.forEach((e) => (e.style.zIndex = '-1'));
     } else if (window.innerWidth <= 768) {
       burger.menu.classList.remove('burger_opened');
       burger.nav.classList.remove('nav_opened');
       scroll.hideScroll();
+
+      setTimeout(() => {
+        if (burger.sliderImages)
+          burger.sliderImages.forEach((e) => (e.style.zIndex = 'auto'));
+      }, 150);
     }
   },
 };
