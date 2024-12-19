ebaef42f408475e77f438f1089b4aa7413e7b8cc
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-19 14:00:40 +0300

    Husky test

---
+++ christmas-shop/src/js/server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+var port=3000
+
+app.get('/',function (req,res) {
+  res.send('Hello Worm!')
+})
+
+app.listen(PORT , function(){
+  console.log(`Server is running on PORT: ${PORT}`);
+  
+})
